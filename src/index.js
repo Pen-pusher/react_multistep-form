214fbfbdfd5ec37b41c33fbfeb0b7817300737a2
Pijush Chakraborty <piyushchakraborty9@gmail.com>
2019-12-18 14:31:49 +0530

    logic implemented

--- src/index.js
+++ src/index.js
@@ -3,26 +3,89 @@ import React from "react";
 import { render } from "react-dom";
 
 import { FiUserPlus } from "react-icons/fi";
+import { MdDone } from "react-icons/md";
 
 // Relative Imports
 import "./styles/normalize.css";
 import "./styles/style.css";
 
-// class Index extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       currentStep: 1,
-//       email: "",
-//       username: "",
-//       password: ""
-//     };
-//   }
-//   render() {
-//     return <Step1 />;
-//   }
-// }
-function Index(props) {
+class Index extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentStep: 1,
+      email: "",
+      username: "",
+      password: ""
+    };
+  }
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const { email, username, password } = this.state;
+    alert(`Your registration detail: \n 
+           Email: ${email} \n 
+           Username: ${username} \n
+           Password: ${password}`);
+  };
+
+  _next = () => {
+    let currentStep = this.state.currentStep;
+    currentStep = currentStep >= 2 ? 3 : currentStep + 1;
+    this.setState({
+      currentStep: currentStep
+    });
+  };
+
+  _previous = () => {
+    let currentStep = this.state.currentStep;
+    currentStep = currentStep <= 1 ? 1 : currentStep - 1;
+    this.setState({
+      currentStep: currentStep
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <form onSubmit={this.handleSubmit}>
+          {/* 
+  
+      */}
+          <Step1
+            currentStep={this.state.currentStep}
+            handleChange={this.handleChange}
+            email={this.state.email}
+            next={this._next}
+          />
+          <Step2
+            currentStep={this.state.currentStep}
+            handleChange={this.handleChange}
+            username={this.state.username}
+            previous={this._previous}
+            next={this._next}
+          />
+          <Step3
+            currentStep={this.state.currentStep}
+            handleChange={this.handleChange}
+            password={this.state.password}
+            previous={this._previous}
+          />
+        </form>
+      </>
+    );
+  }
+}
+function Step1(props) {
+  if (props.currentStep !== 1) {
+    return null;
+  }
   return (
     <div>
       <main>
@@ -98,9 +161,11 @@ function Index(props) {
               <div className="flex">
                 <p className="step">Step 1 of 3</p>
                 <div className="flex">
-                  <button>Previous</button>
+                  <button>Cancel</button>
                   <p className="span-p">or</p>
-                  <button className="next-button">NEXT STEP</button>
+                  <button className="next-button" onClick={props.next}>
+                    NEXT STEP
+                  </button>
                 </div>
               </div>
             </div>
@@ -108,6 +173,127 @@ function Index(props) {
         </body>
       </main>
     </div>
+  );
+}
+function Step2(props) {
+  if (props.currentStep !== 2) {
+    return null;
+  }
+  return (
+    <div>
+      <main>
+        <body>
+          <div className=" wrapper">
+            <h3 className="color-grey">
+              <FiUserPlus />
+              Sign up to create account
+            </h3>
+            <hr />
+            <div className="flex">
+              <h2>
+                {" "}
+                <span className="tick">
+                  <MdDone />
+                </span>
+                Accont Information
+              </h2>
+              <h2>
+                {" "}
+                <span className="circle">2</span>Personal Information
+              </h2>
+              <h2>
+                {" "}
+                <span className="circle">3</span>Payment details
+              </h2>
+            </div>
+            <hr />
+
+            <div>
+              <form>
+                <label htmlFor="email">
+                  Email address
+                  <input
+                    className=""
+                    id="email"
+                    name="email"
+                    type="text"
+                    placeholder="Enter email"
+                    value={props.email}
+                    onChange={props.handleChange}
+                  />
+                </label>
+                <br />
+
+                <label htmlFor="username">
+                  Username
+                  <input
+                    className=""
+                    id="username"
+                    name="username"
+                    type="text"
+                    placeholder="Enter username"
+                    value={props.username}
+                    onChange={props.handleChange}
+                  />
+                </label>
+                <br />
+
+                <label htmlFor="password">
+                  Password
+                  <input
+                    className=""
+                    id="password"
+                    name="password"
+                    type="password"
+                    placeholder="Enter password"
+                    value={props.password}
+                    onChange={props.handleChange}
+                  />
+                </label>
+                <hr />
+                <br />
+              </form>
+
+              <div className="flex">
+                <p className="step">Step 1 of 3</p>
+                <div className="flex">
+                  <button onClick={props.previous}>Previous</button>
+                  <p className="span-p">or</p>
+                  <button className="next-button" onClick={props.next}>
+                    NEXT STEP
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </body>
+      </main>
+    </div>
+  );
+}
+
+function Step3(props) {
+  if (props.currentStep !== 3) {
+    return null;
+  }
+  return (
+    <React.Fragment>
+      <div className="form-group">
+        <label htmlFor="password">Password</label>
+        <input
+          className="form-control"
+          id="password"
+          name="password"
+          type="password"
+          placeholder="Enter password"
+          value={props.password}
+          onChange={props.handleChange}
+        />
+      </div>
+      <button onClick={props.previous}>Previous</button>
+
+      <button className="btn btn-success btn-block">Sign up</button>
+    </React.Fragment>
   );
 }
 
